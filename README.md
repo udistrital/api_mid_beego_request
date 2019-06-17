3baa60a8e134a45a02fa59126206e2f8cae0ac96
jjvargass <jotavargas89@gmail.com>
2019-06-17 16:41:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# api_mid_beego_request
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/udistrital/api_mid_beego_request/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
